432ff1ea9f0d7bdd17b1ae543ac77db9e217c48a
Nguyen Trong Minh <93380914+Minhsuperfragile@users.noreply.github.com>
2023-04-26 16:20:02 +0700

    Add files via upload
    fixed fgets function

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 struct studentInfo
 {
-    char name[30];
+    char name[20];
     char ID[9];
     char birthdate[11];
     float linearAlgebra;
@@ -16,31 +16,47 @@ struct studentInfo
 
 int main (){
 //get number of student:
-    int numberOfStudent;
+    int numberOfStudent,c;
     printf("enter number of student: ");
     scanf("%d",&numberOfStudent);
 //get student info:
-    struct studentInfo *studentArray = calloc(numberOfStudent,sizeof(struct studentInfo));
+    char *p;
+    struct studentInfo studentArray[numberOfStudent];
     for (int i=0;i<numberOfStudent;i++){
+        while ( (c = getchar()) != '\n' && c != EOF ); // remove \n from stream
         printf("Type in student's name: ");
-        scanf("%[^\n]%*c", studentArray[i].name);
-        printf("Type in student's ID");
-        scanf("%s", studentArray[i].ID);
+        fgets(studentArray[i].name,20,stdin);
+        if ((p=strchr(studentArray[i].name, '\n')) != NULL)
+            *p = '\0'; // remove \n from name
+
+        printf("Type in student's ID: ");
+        fgets(studentArray[i].ID,9,stdin);
+        if ((p=strchr(studentArray[i].ID, '\n')) != NULL)
+            *p = '\0'; // remove \n from ID
+        while ( (c = getchar()) != '\n' && c != EOF ); // remove \n from stream
+
         printf("Type in student's birthdate: ");
-        scanf("%s", studentArray[i].birthdate);
+        fgets(studentArray[i].birthdate,11,stdin);
+        if ((p=strchr(studentArray[i].birthdate, '\n')) != NULL)
+            *p = '\0'; // remove \n from birthdate
+        while ( (c = getchar()) != '\n' && c != EOF ); // remove \n from stream
+        
         printf("Type in student's linear grade: ");
         scanf("%f", &studentArray[i].linearAlgebra);
+
         printf("Type in student's calculus grade: ");
         scanf("%f", &studentArray[i].calculus);
+
         printf("Type in student's basic programming grade: ");
-        scanf("%f", &studentArray[i].basicProgramming);
+        scanf("%f", &studentArray[i].basicProgramming);     
+
         studentArray[i].GPA = (studentArray[i].linearAlgebra + studentArray[i].calculus + studentArray[i].basicProgramming)/3;
     }
 //display student info
-    printf("%50s|%15s|%15s|%10s|%10s|%20s|%10s\n","Name","ID","Date of Birth","Linear","Calculus","Basic Programming","GPA");
-    printf("=================================================================================================================\n");
+    printf("%20s|%15s|%15s|%10s|%10s|%20s|%10s\n","Name","ID","Date of Birth","Linear","Calculus","Basic Programming","GPA");
+    printf("===============================================================================================================\n");
     for(int i=0;i<numberOfStudent;i++){
-        printf("%50s|%15s|%15s|%10f|%10f|%20f|%10f\n",            
+        printf("%20s|%15s|%15s|%10f|%10f|%20f|%10f\n",            
             studentArray[i].name,studentArray[i].ID,studentArray[i].birthdate,
             studentArray[i].linearAlgebra,studentArray[i].calculus,studentArray[i].basicProgramming,studentArray[i].GPA);
     }
@@ -53,10 +69,10 @@ int main (){
       exit(1);             
     }        
 
-    fprintf(filePointer,"%50s|%15s|%15s|%10s|%10s|%20s|%10s\n","Name","ID","Date of Birth","Linear","Calculus","Basic Programming","GPA");
-    fprintf(filePointer,"=================================================================================================================\n");
+    fprintf(filePointer,"%20s|%15s|%15s|%10s|%10s|%20s|%10s\n","Name","ID","Date of Birth","Linear","Calculus","Basic Programming","GPA");
+    fprintf(filePointer,"===============================================================================================================\n");
     for(int i=0;i<numberOfStudent;i++){
-        fprintf(filePointer,"%50s|%15s|%15s|%10f|%10f|%20f|%10f\n",            
+        fprintf(filePointer,"%20s|%15s|%15s|%10f|%10f|%20f|%10f\n",            
             studentArray[i].name,studentArray[i].ID,studentArray[i].birthdate,
             studentArray[i].linearAlgebra,studentArray[i].calculus,studentArray[i].basicProgramming,studentArray[i].GPA);       
     }
